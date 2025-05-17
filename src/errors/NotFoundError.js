0d9970d03ba60ceae216d3020a554e23ca93b061
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-05-17 17:39:09 +0200

    feat: delete custom NotFoundError and reroute not found errors to the global prisma error handler

--- src/errors/NotFoundError.js
+++
@@ -1,9 +0,0 @@
-class NotFoundError extends Error {
-	constructor(resource) {
-		super(`${resource} not found`);
-		this.statusCode = 404;
-		this.name = 'NotFoundError';
-	}
-};
-
-module.exports = NotFoundError;
