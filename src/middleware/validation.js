85d1f8e755589e49881ef65100c7025193f82d91
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-06-22 13:22:45 +0200

    chore: remove periods at the end of validation messages

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -3,47 +3,47 @@ const cleanHTML = require('../utils/sanitizeHtml');
 
 const validateRegister = [
 	body('username').trim()
-		.isAlpha().withMessage('Name must only contain letters.')
-		.isLength({ min: 1, max: 30 }).withMessage('Name must be between 1 and 30 characters.'),
+		.isAlpha().withMessage('Name must only contain letters')
+		.isLength({ min: 1, max: 30 }).withMessage('Name must be between 1 and 30 characters'),
 	body('email').normalizeEmail()
-		.isEmail().withMessage('Email must be a valid email address.'),
+		.isEmail().withMessage('Email must be a valid email address'),
 	body('password').trim()
-		.isLength({ min: 8 }).withMessage('Password must be at least 8 characters long.')
-    .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter.')
-    .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter.')
-    .matches(/[0-9]/).withMessage('Password must contain at least one number.'),
+		.isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
+    .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
+    .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+    .matches(/[0-9]/).withMessage('Password must contain at least one number'),
 	body('confirmPass').trim()
 		.custom(async (value, { req }) => {
 			if (value === req.body.password) {
 				return true;
 			}
-			throw new Error('Confirm password must match the password.');
+			throw new Error('Confirm password must match the password');
 		}),
 ];
 
 const validateLogin = [
 	body('email').normalizeEmail()
-		.isEmail().withMessage('Email must be a valid email address.'),
+		.isEmail().withMessage('Email must be a valid email address'),
 	body('password').trim()
-		.notEmpty().withMessage('Password cannot be empty.'),
+		.notEmpty().withMessage('Password cannot be empty'),
 ];
 
 const validateArticle = [
 	body('title').trim()
-		.isLength({ min: 1, max: 70 }).withMessage('Title must be between 1 and 70 characters long.'),
+		.isLength({ min: 1, max: 70 }).withMessage('Title must be between 1 and 70 characters long'),
 	body('content')
 		.customSanitizer((value) => cleanHTML(value))
-		.isLength({ min: 1000, max: 10000 }).withMessage('Content must be between 1000 and 10000 characters long.'),
+		.isLength({ min: 1000, max: 10000 }).withMessage('Content must be between 1000 and 10000 characters long'),
 ];
 
 const validateComment = [
 	body('comment').trim()
-		.isLength({ min: 1, max: 300 }).withMessage('Comment must be between 1 and 300 characters long.'),
+		.isLength({ min: 1, max: 300 }).withMessage('Comment must be between 1 and 300 characters long'),
 ];
 
 const validateCategory = [
 	body('name').trim()
-		.isLength({ min: 1, max: 50 }).withMessage('Category must be between 1 and 50 characters long.'),
+		.isLength({ min: 1, max: 50 }).withMessage('Category must be between 1 and 50 characters long'),
 ];
 
 module.exports = {
