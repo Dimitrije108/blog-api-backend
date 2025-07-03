19bdf53f2b5f3863cb3a316a4e35b2c8536251a7
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-07-03 12:05:58 +0200

    chore: add comment route integration into the main index file

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -1,11 +1,13 @@
-const article = require('./article');
-const user = require('./user');
-const auth = require('./auth');
-const category = require('./category');
+const article = require("./article");
+const user = require("./user");
+const auth = require("./auth");
+const category = require("./category");
+const comment = require("./comment");
 
 module.exports = {
 	article,
 	user,
 	auth,
 	category,
+	comment,
 };
